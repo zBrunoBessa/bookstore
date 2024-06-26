eaa9f45820ed7b3b51fe1bc961346c036a9f9077
Bruno Bessa <beduardo2014@gmail.com>
2024-06-26 14:19:47 -0300

    fix bug

--- bookStore/settings.py
+++ bookStore/settings.py
@@ -141,4 +141,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1'
+]
