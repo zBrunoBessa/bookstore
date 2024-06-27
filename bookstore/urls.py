013c23a5a447bfda895fde3d71daec6ffe379eda
Bruno Bessa <beduardo2014@gmail.com>
2024-06-27 16:48:57 -0300

    feat: templates

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
@@ -26,4 +27,6 @@ urlpatterns = [
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
